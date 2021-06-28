c35276b0de604235f38e21daec5b4d12579afa80
Jeremy Grecu <jeremy.grecu@sap.com>
2021-06-28 14:42:08 +0100

    JBA - Cinema Room Manager - Cinema Room Manager stage 3 / 5

--- task/src/cinema/Cinema.java
+++ task/src/cinema/Cinema.java
@@ -5,31 +5,75 @@ import java.util.Scanner;
 public class Cinema {
 
     public static void main(String[] args) {
-//        System.out.println("Cinema:");
-//        System.out.println("  1 2 3 4 5 6 7 8");
-//        for (int i = 0; i < 7; i++) {
-//            System.out.print((i + 1) + " ");
-//            for (int j = 0; j < 8; j++) {
-//                System.out.print("S ");
-//            }
-//            System.out.println();
-//        }
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of rows: ");
         int numberOfRows = scanner.nextInt();
         System.out.println("Enter the number of seats in each row: ");
         int seatsPerRow = scanner.nextInt();
-        scanner.close();
 
-        int totalIncome = 0;
+
         int totalNumberOfSeats = numberOfRows * seatsPerRow;
-        if (totalNumberOfSeats <= 60) {
-            totalIncome = totalNumberOfSeats * 10;
-        } else {
-            totalIncome = (numberOfRows / 2 * seatsPerRow * 10) + ((numberOfRows - numberOfRows / 2) * seatsPerRow * 8);
+
+        String[][] cinemaRoom = new String[numberOfRows][seatsPerRow];
+
+        for (int i = 0; i < cinemaRoom.length; i++) {
+            for (int j = 0; j < cinemaRoom[i].length; j++) {
+                cinemaRoom[i][j] = "S";
+            }
         }
 
-        System.out.println("Total income:");
-        System.out.println("$" + totalIncome);
+        printCinemaRoom(cinemaRoom);
+
+        System.out.println();
+        System.out.println("Enter a row number: ");
+        int choiceRow = scanner.nextInt();
+        System.out.println("Enter a seat number in that row: ");
+        int choiceSeat = scanner.nextInt();
+        scanner.close();
+
+        cinemaRoom[choiceRow - 1][choiceSeat - 1] = "B";
+
+        int ticketPrice = 0;
+        if (totalNumberOfSeats <= 60) {
+            ticketPrice = 10;
+        } else if (choiceRow <= numberOfRows / 2) {
+            ticketPrice = 10;
+        } else {
+            ticketPrice = 8;
+        }
+
+        System.out.println("\nTicket price: $" + ticketPrice + "\n");
+
+        printCinemaRoom(cinemaRoom);
+
+//        int totalIncome = 0;
+//        if (totalNumberOfSeats <= 60) {
+//            totalIncome = totalNumberOfSeats * 10;
+//        } else {
+//            totalIncome = (numberOfRows / 2 * seatsPerRow * 10) + ((numberOfRows - numberOfRows / 2) * seatsPerRow * 8);
+//        }
+//
+//        System.out.println("Total income:");
+//        System.out.println("$" + totalIncome);
+    }
+
+    private static void printCinemaRoom(String[][] cinemaRoom) {
+        System.out.println("Cinema:");
+        for (int i = 0; i <= cinemaRoom[0].length; i++) {
+            if (i == 0) {
+                System.out.print("  ");
+            } else if (i < cinemaRoom[0].length) {
+                System.out.print(i + " ");
+            } else {
+                System.out.println(i);
+            }
+        }
+        for (int i = 0; i < cinemaRoom.length; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < cinemaRoom[i].length; j++) {
+                System.out.print(cinemaRoom[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
